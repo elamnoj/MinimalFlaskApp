c94f68dca2a2d7dd1fca052b7affe73f9dee7872
elamnoj <male.jonathan@gmail.com>
2021-05-27 13:26:39 -0500

    first commit

--- app/routes.py
+++ app/routes.py
@@ -2,16 +2,33 @@ from app import app, db
 from flask import render_template, request, redirect, url_for, flash
 from app.models import Post, User, Submit
 from flask_login import login_user, logout_user
+import plotly
+from plotly import express as px
+import plotly.io as pio
 
-@app.route('/',methods=['GET', 'POST'])
+
+
+import pandas as pd
+import numpy as np
+import json
+
+
+
+
+@app.route('/')
 def home():
-    if request.method == 'POST':
-        p = Post(email='derek@codingtemple.com', body=request.form.get('body_text'))
-        db.session.add(p)
-        db.session.commit()
-        flash('Sucess!')
-        return redirect(url_for('home'))
-    return render_template('index.html')
+    air = pd.read_csv(r'listings.csv')
+
+    # pio.renderers.default = 'browser'
+    fig = px.scatter_mapbox(air, lat="latitude", lon="longitude", hover_name="neighbourhood_cleansed", hover_data=[
+                            "room_type", "price"], color="neighbourhood_cleansed", zoom=11, height=400)
+
+    fig.update_layout(mapbox_style="carto-darkmatter")
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
+    graphJSON = json.dumps(fig.show(), cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('index.html', graphJSON=graphJSON)
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
